8ee9cb4720a1da38d02be0ba6c1874fd979fee74
brohanminfy <boddupally.rohan@minfytech.com>
2025-07-21 14:45:40 +0530

    Revert "Modularity"
    This reverts commit a3461419d11f927ef8ae2a1521db966994a14df8.

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,7 +1,7 @@
 import admin from 'firebase-admin';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const serviceAccount = require('./fireadmin.json');
+const serviceAccount = require('./serviceAccount.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
